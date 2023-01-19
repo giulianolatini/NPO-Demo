03ad90435da13e506889d23cecb0c2018e5af7dc
Giuliano Latini <latini.giuliano@gmail.com>
2023-01-19 18:28:33 +0100

    Add creation RG

---
+++ Scripts/CreateRG.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z ${location:?error&exit} ]; 
+then 
+    echo "The location is unset, exiting";
+    break;
+fi
+if [ -z ${RG:?error&exit} ]; 
+then 
+    echo "the ResourceGroup is unset, exiting";
+    break;
+fi
+
+# Create Resource Group and it set as default
+az group create -l $location -n $RG
+az configure --defaults location=$location group=$RG
